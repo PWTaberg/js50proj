d7b6fd071d95c358cb783181c40a51008899cf5a
Peter W <pwelliver@yahoo.com>
2020-12-23 16:27:15 +0100

    proj 10 dad jokes, fetch

---
+++ proj10 dad jokes/script.js
@@ -0,0 +1,37 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+/* using async / await */
+async function generateJoke() {
+	const config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	};
+
+	const result = await fetch(`https://icanhazdadjoke.com`, config);
+
+	const data = await result.json();
+
+	jokeEl.innerHTML = data.joke;
+}
+
+/* using promises
+function generateJoke() {
+	const config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	};
+
+	fetch(`https://icanhazdadjoke.com`, config)
+		.then((res) => res.json())
+		.then((data) => {
+			jokeEl.innerHTML = data.joke;
+		});
+}
+*/

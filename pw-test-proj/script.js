ddb7614943d3009ada82316b5f455332f5d3fb76
Peter W <pwelliver@yahoo.com>
2021-01-09 20:11:19 +0100

    added more comments

--- pw-test-proj/script.js
+++ pw-test-proj/script.js
@@ -13,71 +13,7 @@ const readNumberStrDec = document.getElementById('readNumberStrDec');
 const resulTimeInput = document.getElementById('resultTimeInput');
 const secToTime = document.getElementById('secToTime');
 
-secToTime.addEventListener('click', (e) => {
-	e.preventDefault();
-	console.log(resultNumber);
-	console.log(resultNumber.value);
-
-	readNumber.innerText = 'Missing input';
-
-	// validation
-	let numberIsEntered = true;
-
-	// Check that timestring format is OK
-
-	if (validNumberStr(resultNumber.value) === true) {
-		readNumber.innerText = resultNumber.value;
-	} else {
-		numberIsEntered = false;
-		readNumber.innerText = 'Missing input';
-	}
-
-	if (numberIsEntered) {
-		// Convert number to time string
-		let resultTimeStr = secToTimeStr(resultNumber.value);
-
-		resulTimeInput.value = resultTimeStr;
-		readNumberStr.innerText = resultTimeStr;
-
-		let useDecimals = true;
-		resultTimeStr = secToTimeStr(resultNumber.value, useDecimals);
-
-		readNumberStrDec.innerText = resultTimeStr;
-	}
-});
-
-const secToTimeStr = (numberVal, useDecimals = false) => {
-	let timeLeft = numberVal;
-	let hours = Math.floor(timeLeft / (60 * 60));
-	timeLeft = timeLeft - hours * 60 * 60;
-
-	let minutes = Math.floor(timeLeft / 60);
-	timeLeft = timeLeft - minutes * 60;
-
-	let seconds = Math.floor(timeLeft);
-	timeLeft = timeLeft - seconds;
-
-	let tenths = Math.floor(timeLeft * 10);
-	timeLeft = timeLeft - tenths / 10;
-
-	let hundreds = Math.floor(timeLeft * 100);
-	timeLeft = timeLeft - hundreds / 100;
-
-	let hhStr = hours < 10 ? '0' + hours.toString() : hours.toString();
-	let mmStr = minutes < 10 ? '0' + minutes.toString() : minutes.toString();
-	let ssStr = seconds < 10 ? '0' + seconds.toString() : seconds.toString();
-	let tenthsStr = tenths.toString();
-	let hundredsStr = hundreds.toString();
-
-	let timeString = '00:00:00';
-	if (useDecimals === true) {
-		timeString = `${hhStr}:${mmStr}:${ssStr}.${tenthsStr}${hundredsStr}`;
-	} else {
-		timeString = `${hhStr}:${mmStr}:${ssStr}`;
-	}
-	return timeString;
-};
-
+// Time String to Number
 timeToSec.addEventListener('click', (e) => {
 	e.preventDefault();
 	console.log(maxTime);
@@ -122,14 +58,12 @@ timeToSec.addEventListener('click', (e) => {
 	}
 });
 
+// Used for validating that a complete Time String is entered -  modern JS
 const validTimeStr = (timeStr) => {
 	return timeStr.length === 8 ? true : false;
 };
 
-const validNumberStr = (numberStr) => {
-	return numberStr.length > 0 ? true : false;
-};
-
+// Used for validating that a complete Time String is entered - old fashioned JS
 function validTimeStr2(timeStr) {
 	if (timeStr.length === 8) {
 		return true;
@@ -138,10 +72,17 @@ function validTimeStr2(timeStr) {
 	}
 }
 
+// Used for validating that a number is entered
+const validNumberStr = (numberStr) => {
+	return numberStr.length > 0 ? true : false;
+};
+
+// Used for validating that min value is less than max value
 const validMinMax = (minVal, maxVal) => {
 	return minVal < maxVal ? true : false;
 };
 
+// Used for converting a time string to a number with seconds
 const timeInSec = (timeStr) => {
 	// split hh.MM.ss into an array
 	const timeArray = timeStr.split(':');
@@ -157,4 +98,71 @@ const timeInSec = (timeStr) => {
 		seconds = hhNumber * 60 * 60 + mmNumber * 60 + ssNumber;
 	}
 	return seconds;
+};
+
+//Number to Time String
+secToTime.addEventListener('click', (e) => {
+	e.preventDefault();
+	console.log(resultNumber);
+	console.log(resultNumber.value);
+
+	readNumber.innerText = 'Missing input';
+
+	// validation
+	let numberIsEntered = true;
+
+	// Check that timestring format is OK
+
+	if (validNumberStr(resultNumber.value) === true) {
+		readNumber.innerText = resultNumber.value;
+	} else {
+		numberIsEntered = false;
+		readNumber.innerText = 'Missing input';
+	}
+
+	if (numberIsEntered) {
+		// Convert number to time string
+		let resultTimeStr = secToTimeStr(resultNumber.value);
+
+		resulTimeInput.value = resultTimeStr;
+		readNumberStr.innerText = resultTimeStr;
+
+		let useDecimals = true;
+		resultTimeStr = secToTimeStr(resultNumber.value, useDecimals);
+
+		readNumberStrDec.innerText = resultTimeStr;
+	}
+});
+
+// Used for converting a number with seconds to a time string
+const secToTimeStr = (numberVal, useDecimals = false) => {
+	let timeLeft = numberVal;
+	let hours = Math.floor(timeLeft / (60 * 60));
+	timeLeft = timeLeft - hours * 60 * 60;
+
+	let minutes = Math.floor(timeLeft / 60);
+	timeLeft = timeLeft - minutes * 60;
+
+	let seconds = Math.floor(timeLeft);
+	timeLeft = timeLeft - seconds;
+
+	let tenths = Math.floor(timeLeft * 10);
+	timeLeft = timeLeft - tenths / 10;
+
+	let hundreds = Math.floor(timeLeft * 100);
+	timeLeft = timeLeft - hundreds / 100;
+
+	let hhStr = hours < 10 ? '0' + hours.toString() : hours.toString();
+	let mmStr = minutes < 10 ? '0' + minutes.toString() : minutes.toString();
+	let ssStr = seconds < 10 ? '0' + seconds.toString() : seconds.toString();
+	let tenthsStr = tenths.toString();
+	let hundredsStr = hundreds.toString();
+
+	let timeString = '00:00:00';
+	if (useDecimals === true) {
+		timeString = `${hhStr}:${mmStr}:${ssStr}.${tenthsStr}${hundredsStr}`;
+	} else {
+		timeString = `${hhStr}:${mmStr}:${ssStr}`;
+	}
+	return timeString;
 };

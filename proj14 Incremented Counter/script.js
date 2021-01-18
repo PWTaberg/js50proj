464cb854c73cdce461e2e650edd8b9777f432041
Peter W <pwelliver@yahoo.com>
2021-01-18 12:35:58 +0100

    proj 14 incremented counters

---
+++ proj14 Incremented Counter/script.js
@@ -0,0 +1,31 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+	counter.innerText = '0';
+	//data-target
+	//const dataTarget = +counter.getAttribute('data-target');
+	// displayed number
+	//const dispNumber = +counter.innerText;
+
+	const updateCounter = () => {
+		//data-target
+		const dataTarget = Number(counter.getAttribute('data-target'));
+		//	const dataTarget = Number(counter.getAttribute('data-target'));
+
+		// displayed number
+		const dispNumber = Number(counter.innerText);
+		//const dispNumber = +counter.innerText;
+
+		const increment = dataTarget / 500;
+
+		if (dispNumber < dataTarget) {
+			// update innerText with an increment
+			counter.innerText = `${Math.ceil(dispNumber + increment)}`;
+			setTimeout(updateCounter, 1);
+		} else {
+			counter.innerText = dataTarget;
+		}
+	};
+
+	updateCounter();
+});
--- pw-test-proj/script.js
+++ pw-test-proj/script.js
@@ -5,6 +5,8 @@ const minTimeSec = document.getElementById('minTimeSec');
 const readMaxTimeStr = document.getElementById('readMaxTimeStr');
 const readMinTimeStr = document.getElementById('readMinTimeStr');
 const timeToSec = document.getElementById('timeToSec');
+const limitBtn = document.getElementById('limitBtn');
+const limitTxt = document.getElementById('limitTxt');
 
 const resultNumber = document.getElementById('resultNumber');
 const readNumber = document.getElementById('readNumber');
@@ -13,33 +15,35 @@ const readNumberStrDec = document.getElementById('readNumberStrDec');
 const resulTimeInput = document.getElementById('resultTimeInput');
 const secToTime = document.getElementById('secToTime');
 
+let useLimit = false;
+limitTxt.innerText = 'NO  LIMIT';
+
 // Time String to Number
 timeToSec.addEventListener('click', (e) => {
 	e.preventDefault();
-	console.log(maxTime);
-	console.log(maxTime.value);
-
 	// validation
 	let timeFormatIsOK = true;
-	let minMaxIsOK = true;
 	let minValueToLow = false;
 	let minValueToHigh = false;
 	let maxValueToLow = false;
 	let maxValueToHigh = false;
+
+	console.log(maxTime);
+	console.log(maxTime.value);
 
 	// Check that timestring format is OK
 
 	if (validTimeStr(maxTime.value) === true) {
 		readMaxTimeStr.innerText = maxTime.value;
 	} else {
-		timeFormatIsOk = false;
+		timeFormatIsOK = false;
 		readMaxTimeStr.innerText = 'Invalid time format, use hh:mm:ss ';
 	}
 
 	if (validTimeStr(minTime.value) === true) {
 		readMinTimeStr.innerText = minTime.value;
 	} else {
-		timeFormatIsOk = false;
+		timeFormatIsOK = false;
 		minTimeSec.innerText = 'Invalid time format, use hh:mm:ss ';
 	}
 
@@ -49,24 +53,18 @@ timeToSec.addEventListener('click', (e) => {
 		let maxTimeInSec = timeInSec(maxTime.value);
 
 		if (toHigh(minTimeInSec) === true) {
-			console.log('to High');
 			minValueToHigh = true;
 		}
 
 		if (toLow(minTimeInSec) === true) {
-			console.log('to Low');
 			minValueToLow = true;
 		}
 
 		if (toHigh(maxTimeInSec) === true) {
-			console.log('to High');
-
 			maxValueToHigh = true;
 		}
 
 		if (toLow(maxTimeInSec) === true) {
-			console.log('to low');
-
 			maxValueToLow = true;
 		}
 		// check if min < max
@@ -77,25 +75,43 @@ timeToSec.addEventListener('click', (e) => {
 			minMaxIsOK = false;
 			maxTimeSec.innerText = 0;
 			minTimeSec.innerText = 0;
-			maxTimeSec.innerText = 'Min Time should be less than Max Time';
-			minTimeSec.innerText = 'Min Time should be less than Max Time';
+			readMaxTimeStr.innerText = 'Min Time should be less than Max Time';
+			readMinTimeStr.innerText = 'Min Time should be less than Max Time';
 		}
-
-		// check if any value is to low
-		if (minValueToLow === true) {
-			minTimeSec.innerText = 'The race is about running, not flying';
-		}
-		if (maxValueToLow === true) {
-			maxTimeSec.innerText = 'The race is about running, not flying';
-		}
-		// check if any value is to high
-		if (minValueToHigh === true) {
-			minTimeSec.innerText = 'The race is about running, not crawling';
-		}
-		if (maxValueToHigh === true) {
-			maxTimeSec.innerText = 'The race is about running, not crawling';
+		if (useLimit) {
+			// check if any value is to low
+			if (minValueToLow === true) {
+				readMinTimeStr.innerText =
+					'The race is about running, not flying';
+			}
+			if (maxValueToLow === true) {
+				readMaxTimeStr.innerText =
+					'The race is about running, not flying';
+			}
+			// check if any value is to high
+			if (minValueToHigh === true) {
+				readMinTimeStr.innerText =
+					'The race is about running, not crawling';
+			}
+			if (maxValueToHigh === true) {
+				readMaxTimeStr.innerText =
+					'The race is about running, not crawling';
+			}
 		}
 	}
+});
+
+// Toggle Limit Button
+
+limitBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	// toggle useLimit
+	useLimit === true ? (useLimit = false) : (useLimit = true);
+
+	// toggle limitTxt
+	let text = '';
+	useLimit === true ? (text = 'USE LIMIT') : (text = 'NO LIMIT');
+	limitTxt.innerText = text;
 });
 
 // Used for validating that a complete Time String is entered -  modern JS
@@ -124,13 +140,11 @@ const validMinMax = (minVal, maxVal) => {
 
 // Used for validating that value not lower than 165 ~ 2.45 min
 const toLow = (numVal) => {
-	console.log('toLow ', numVal);
 	return numVal < 165 ? true : false;
 };
 
 // Used for validating that value is not greater than 420 ~ 7 min
 const toHigh = (numVal) => {
-	console.log('toHigh ', numVal);
 	return numVal > 420 ? true : false;
 };
 
@@ -218,3 +232,91 @@ const secToTimeStr = (numberVal, useDecimals = false) => {
 	}
 	return timeString;
 };
+
+const resultList = [
+	{
+		name: 'Bettan',
+		time: 12,
+	},
+	{
+		name: 'Anna',
+		time: 7,
+	},
+	{
+		name: 'Stina',
+		time: 1,
+	},
+	{
+		name: 'Gullan',
+		time: 4,
+	},
+	{
+		name: 'Jenny',
+		time: 6,
+	},
+];
+
+const resultSort = (unordered) => {
+	console.log(unordered);
+	const sorted = unordered.sort((a, b) => {
+		console.log(a, b);
+		return a.time - b.time;
+	});
+	return sorted;
+};
+
+const sortedList = resultSort(resultList);
+console.log(sortedList);
+
+// Input anmälningstid, parameters, id ?
+const getLegTimes = (distance) => {
+	// Create an empty array with en entry for each splitTime
+	const legs = [...Array(distance).keys()];
+
+	let splitTime = 0;
+	let prevSplitTime = 0;
+	let subTotal = 0;
+
+	// For each split calculate the time
+	// Keep the new split and the sub total (accumulated time)
+	const legTimes = legs.map((leg, index) => {
+		console.log('leg: ', leg);
+
+		if (index === 0) {
+			// First split
+			splitTime = 10 + index;
+			prevSplitTime = splitTime;
+			subTotal = splitTime;
+		} else {
+			// Following split
+			splitTime = prevSplitTime += 1;
+			prevSplitTime = splitTime;
+			subTotal = subTotal + splitTime;
+		}
+		console.log(splitTime);
+		return { splitTime, subTotal };
+	});
+
+	console.log(legTimes);
+
+	return legTimes;
+};
+
+// Beövs Inte
+const getResultTime = (splitTimes) => {
+	console.log(splitTimes);
+
+	const splits = splitTimes.map((entry) => {
+		return entry.splitTime;
+	});
+	const resultTime = splits.reduce((acc, value) => acc + value);
+	console.log('Result ', resultTime);
+
+	return resultTime;
+};
+
+const myLegTimes = getLegTimes(6);
+console.log('my legTimes ', myLegTimes);
+
+const myResult = getResultTime(myLegTimes);
+console.log(myResult);

18fb9c8d4939b2284eb49f3c63c391ee340dbfb8
Peter W <pwelliver@yahoo.com>
2021-02-01 21:05:57 +0100

    modified toast

--- proj15 and more/proj 26 toast note/script.js
+++ proj15 and more/proj 26 toast note/script.js
@@ -1,8 +1,12 @@
 // Simplify, skip random
 
-const button = document.getElementById('button');
+const buttonInfo = document.getElementById('buttonInfo');
+const buttonError = document.getElementById('buttonError');
+const buttonSuccess = document.getElementById('buttonSuccess');
+
 const toasts = document.getElementById('toasts');
 
+// Not used
 const messages = [
 	'Message One',
 	'Message Two',
@@ -10,9 +14,19 @@ const messages = [
 	'Message Four',
 ];
 
+// Not really needed
 const types = ['info', 'success', 'error'];
 
-button.addEventListener('click', () => createNotification());
+// button.addEventListener('click', () => createNotification());
+buttonInfo.addEventListener('click', () =>
+	createNotification('Info message', 'info')
+);
+buttonSuccess.addEventListener('click', () =>
+	createNotification('Success message', 'success')
+);
+buttonError.addEventListener('click', () =>
+	createNotification('Error message', 'error')
+);
 
 function createNotification(message = null, type = null) {
 	// Create div
@@ -20,22 +34,27 @@ function createNotification(message = null, type = null) {
 	// add class toast
 	notif.classList.add('toast');
 	// add message
-	notif.classList.add(type ? type : getRandomType());
 
-	notif.innerText = message ? message : getRandomMessage();
+	notif.classList.add(type);
 
+	notif.innerText = message;
+
+	// Add tost to toasts div
 	toasts.appendChild(notif);
 
+	// Make it disapear in 3 secs
 	setTimeout(() => {
 		notif.remove();
 	}, 3000);
 }
 
+// Not used
 function getRandomMessage() {
 	//  return a random message
 	return messages[Math.floor(Math.random() * messages.length)];
 }
 
+// Not used
 function getRandomType() {
 	return types[Math.floor(Math.random() * types.length)];
 }
